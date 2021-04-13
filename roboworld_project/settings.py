421902d0861f5a6a4beeccc9c3b25b4c265de936
Christian González <79123808+grchristian@users.noreply.github.com>
2021-04-12 21:36:45 -0500

    update db to postgresql

--- roboworld_project/settings.py
+++ roboworld_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'roboworld_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'roboworlddb',
+        'USER': 'admindb',
+        'PASSWORD': 'equipo1db',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
