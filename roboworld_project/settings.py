75314a65e2dc4ff3ac38327f8d5b5de5257bab39
Christian González <79123808+grchristian@users.noreply.github.com>
2021-04-12 22:04:26 -0500

    Update settings.py

--- roboworld_project/settings.py
+++ roboworld_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'roboworlddb',
         'USER': 'admindb',
-        'PASSWORD': 'equipo1db',
+        'PASSWORD': 'Adminequipo1',
         'HOST': 'localhost',
         'PORT': '5432',
     }
