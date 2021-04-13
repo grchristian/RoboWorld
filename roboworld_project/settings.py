6d1ef3a05f06679f08be823d48bbb74934612311
Christian González <79123808+grchristian@users.noreply.github.com>
2021-04-12 22:24:53 -0500

    Update settings.py

--- roboworld_project/settings.py
+++ roboworld_project/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 import os
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'roboworld_app/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
