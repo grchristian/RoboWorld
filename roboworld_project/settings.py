044fdf9cd5706aa3df7a021da04ad90de5094bc7
Christian González <79123808+grchristian@users.noreply.github.com>
2021-04-12 21:59:44 -0500

    Update settings.py

--- roboworld_project/settings.py
+++ roboworld_project/settings.py
@@ -124,5 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
