0805f401213103dc3e9d68a4e42e90c634b04083
Christian González <a01657929@itesm.mx>
2021-04-13 18:53:30 -0500

    Update urls.py

--- roboworld_app/urls.py
+++ roboworld_app/urls.py
@@ -5,12 +5,6 @@ urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('stats/', views.stats, name="stats"),
     path('micuenta/', views.micuenta, name="micuenta"),
-    path('juego_unity/', views.juego_unity, name="juego_unity"),
-
-    '''
-    path('login/', views.login, name="login"),
-    path('logged_out/', views.logged_out, name="logged_out"),
-    path('score/', views.score, name="score"),
-    '''
+    path('juego_unity/', views.juego_unity, name="juego_unity")
 ]
 
