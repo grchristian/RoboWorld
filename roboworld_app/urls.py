f473d332e439e2743bf69751bb8c72e1efdce017
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-16 18:26:23 -0500

    Update urls.py

--- roboworld_app/urls.py
+++ roboworld_app/urls.py
@@ -6,12 +6,19 @@ from . import views
 urlpatterns = [
 
     path('', views.inicio, name="inicio"),
+    path('proceso',views.proceso, name = 'proceso'),
+    path('datos',views.datos, name = 'datos'),
     path('stats/', views.stats, name="stats"),
     path('micuenta/', views.micuenta, name="micuenta"),
     path('juego_unity/', views.juego_unity, name="juego_unity"),
     path('iniciar_sesion/', views.iniciar_sesion, name="iniciar_sesion"),
     path('buscaJugadorBody', views.buscaJugadorBody, name='buscaJugadorBody'),
     path('ejemploSQL', views.ejemploSQL, name='ejemploSQL'),
+    path('score',views.score, name = 'score'),
+]
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 '''
