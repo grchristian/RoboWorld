499235343e81695e114d908a14735da397c84152
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-16 18:52:53 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -98,7 +98,7 @@ def ejemploSQL(request):
             database = "dataroboworld"
         )
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM juego_reto;")
+        cursor.execute("SELECT * FROM roboworld_app_reto;")
         rows = cursor.fetchall()
         for row in rows:
             if row[1] == jugador_nombre:
