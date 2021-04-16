fd7288a5be92725de92ad12ec20b35f0df287429
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-16 18:11:03 -0500

    Update models.py

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import User
 
 class User(models.Model):
     nombre = models.CharField(max_length=200)
+    
+class Reto(models.Model):
+    nombre = models.CharField(max_length=30)
+    minutos_jugados = models.IntegerField()
