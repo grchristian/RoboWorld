8dabd84d33009af552261f57500efa07ade8b836
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-16 17:55:11 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from json import loads
 
 
 '''
@@ -22,10 +24,11 @@ def juego_unity(request):
 def iniciar_sesion(request):
     return render(request, "roboworld_app/iniciar_sesion.html")
 
-def micuenta(request):
-    nombre = "Rebeca"
+def proceso(request):
+    nombre = request.POST['Rebeca']
+    nombre=nombre.upper()
     
-    return render(request, "roboworld_app/micuenta.html", {"nombre":nombre}) 
+    return render(request, "roboworld_app/proceso.html", {"nombre":nombre}) 
 
 
 @csrf_exempt
@@ -34,8 +37,66 @@ def micuenta(request):
     session={
     num_engranes = "43"
     min_jugados = "53"
-    veces_jugadas = "5"}
+    veces_jugadas = "5"
+    }
     return JsonResponse(session)
+
+@csrf_exempt
+
+def buscaJugadorBody(request):
+    body_unicode = request.body.decode('utf-8')
+    body_json = loads(body_unicode) #convertir de string a JSON
+    jugador_nombre = body_json['usuario']
+    resultados = Reto.objects.filter(nombre=jugador_nombre)  #select * from Reto where nombre = jugador_nombre
+    
+    nombre = resultados[0].nombre
+    score = resultados[0].minutos_jugados
+    '''
+    nombre = "Marco"
+    score = "5000"
+    '''
+    retorno = {"nombreUsuario":nombre,
+        "score":score}
+    return JsonResponse(retorno)
+
+@csrf_exempt
+def ejemploSQL(request):
+    body_unicode = request.body.decode('utf-8')
+    body_json = loads(body_unicode) #convertir de string a JSON
+    jugador_nombre = body_json['usuario']
+    nombre = ""
+    score = ""
+
+    try:
+        connection = psycopg2.connect(
+            user = "marcobglz",
+            password = "marc0.bot",
+            host = "localhost",
+            port = "5432",
+            database = "steam"
+        )
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM juego_reto;")
+        rows = cursor.fetchall()
+        for row in rows:
+            if row[1] == jugador_nombre:
+                nombre = row[1]
+                score = row[2]
+            print(row)
+    
+    except(Exception, psycopg2.Error) as error:
+        print('Error connecting to PostgreSQL database', error)
+        connection = None
+    
+    finally:
+        if(connection != None):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is now closed")
+    retorno = {"nombreUsuario":nombre,
+        "score":score}
+    return JsonResponse(retorno)
+
 '''
 def micuenta(request):
     nombre = "Rebeca"
