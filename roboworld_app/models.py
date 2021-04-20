74762858b6e95678cfbf66a916dc3bfa1d66eaf7
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-20 18:14:39 -0500

    Update models.py

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -3,19 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class User(models.Model):
-    Userid = models.AutoField(primary_key=True)
+class Usuario(models.Model):
+    #UserioId = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=30)
     contraseña = models.CharField(max_length=30)
     
 class Reto(models.Model):
-    Userid = models.ForeignKey(User, on_delete=models.CASCADE)
+    Userid = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     minutos_jugados = models.IntegerField()
     minimo = models.IntegerField()
 
 
 class Level(models.Model):
-    Nivelid = models.AutoField(primary_key=True)
+    #Nivelid = models.AutoField(primary_key=True)
     level_number = models.IntegerField()
     enemigo = models.CharField(max_length=30)
     dificultad = models.IntegerField()
@@ -29,8 +29,8 @@ class Engranes(models.Model):
 
 
 class Sesion(models.Model):
-    SessionId = models.IntegerField()
-    UserID = models.IntegerField()
+    #SessionId = models.IntegerField()
+   # UserID = models.IntegerField()
     started = models.IntegerField()
     ended = models.IntegerField()
     score = models.IntegerField()
@@ -38,14 +38,14 @@ class Sesion(models.Model):
 
 
 class Recompensas(models.Model):
-    recompensasid = models.IntegerField()
+   # recompensasid = models.IntegerField()
     engranes_necesarios = models.IntegerField()
     top_score_global = models.IntegerField()
     top_five = models.IntegerField()
 
 
 class Prueba(models.Model):
-    Levelid = models.IntegerField()
+    #Levelid = models.IntegerField()
     SesionID = models.IntegerField()
     success = models.BooleanField() 
 
