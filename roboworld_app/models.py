4002b821b56bc16e9d7e0a87df513e5e26484146
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-20 11:35:39 -0500

    Update models.py

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -9,3 +9,43 @@ class User(models.Model):
 class Reto(models.Model):
     nombre = models.CharField(max_length=30)
     minutos_jugados = models.IntegerField()
+
+'''
+class Level(models.Model):
+    Levelid = models.IntegerField()
+    level_number = models.IntegerField()
+    enemigo = models.CharField(max_length=30)
+    dificultad = models.IntegerField()
+    duracion_indivudual = models.IntegerField()
+'''    
+
+'''
+class Engranes(models.Model):
+    Engraneid = models.IntegerField()
+    sessionObtained = models.IntegerField()
+    number = models.IntegerField()
+'''  
+'''
+class Session(models.Model):
+    SessionId = models.IntegerField()
+    UserID = models.IntegerField()
+    started = models.IntegerField()
+    ended = models.IntegerField()
+    score = models.IntegerField()
+'''
+
+'''
+class Recompensas(models.Model):
+    recompensasid = models.IntegerField()
+    engranes_necesarios = models.IntegerField()
+    top_score_global = models.IntegerField()
+    top_five = models.IntegerField()
+'''
+
+'''
+class Try(models.Model):
+    Levelid = models.IntegerField()
+    SessionID = models.IntegerField()
+    success = models.booleanField() //duda
+'''
+    
