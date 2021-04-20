4e94198375966530e3fc3b727fe76d3824f197d9
rebegula <75821729+rebegula@users.noreply.github.com>
2021-04-20 11:37:43 -0500

    Update admin.py

--- roboworld_app/admin.py
+++ roboworld_app/admin.py
@@ -6,6 +6,21 @@ from django.contrib import admin
 
 from .models import User
 from .models import Reto
+'''
+from .models import Level
+from .models import Engranes
+from .models import Session
+from .models import Recompensas
+from .models import Try
+'''
 
 admin.site.register(User)
 admin.site.register(Reto)
+
+'''
+admin.site.register(Level)
+admin.site.register(Engranes)
+admin.site.register(Session)
+admin.site.register(Recompensas)
+admin.site.register(Try)
+'''
