9b978a5aefcfbd6ab6e6d137f9caaab9e259b740
Christian González <a01657929@itesm.mx>
2021-04-20 23:23:42 -0500

    Update urls.py

--- roboworld_app/urls.py
+++ roboworld_app/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
 
 urlpatterns = [
-
     path('', views.inicio, name="inicio"),
     path('proceso',views.proceso, name = 'proceso'),
     path('datos',views.datos, name = 'datos'),
