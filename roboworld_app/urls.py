a948ddd9bfbce14932f7e3f747ab8e4140b1f9fe
Christian González <a01657929@itesm.mx>
2021-04-21 18:15:00 -0500

    register delete

--- roboworld_app/urls.py
+++ roboworld_app/urls.py
@@ -3,23 +3,24 @@ from . import views
 
 
 urlpatterns = [
+    #finales
     path('', views.inicio, name="inicio"),
+    path('stats/', views.stats, name="stats"),
+    path('juego_unity/', views.juego_unity, name="juego_unity"),
+    path('cuenta_usuario',views.cuenta_usuario, name = 'cuenta_usuario'),
+
     path('proceso',views.proceso, name = 'proceso'),
     path('datos',views.datos, name = 'datos'),
-    path('stats/', views.stats, name="stats"),
     path('micuenta/', views.micuenta, name="micuenta"),
-    path('juego_unity/', views.juego_unity, name="juego_unity"),
     path('iniciar_sesion/', views.iniciar_sesion, name="iniciar_sesion"),
     path('buscaJugadorBody', views.buscaJugadorBody, name='buscaJugadorBody'),
     path('ejemploSQL', views.ejemploSQL, name='ejemploSQL'),
     path('score',views.score, name = 'score'),
     path('grafica',views.grafica, name = 'grafica'),
 
+    #ejemplos clase
     path('index2',views.index2, name = 'index2'),
-    path('proceso2',views.proceso2, name = 'proceso2'),
-    
-    path('cuenta_usuario',views.cuenta_usuario, name = 'cuenta_usuario'),
-    path('register/', views.register, name='register')
+    path('proceso2',views.proceso2, name = 'proceso2')
 ]
 
 
