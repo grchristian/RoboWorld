6904f80669e272620af04929a869ca05d556b912
Christian González <a01657929@itesm.mx>
2021-04-23 18:28:31 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -52,7 +52,7 @@ def cuenta_usuario(request):
 def cuenta_usuario(request):
 
     #send.user = request.user # set the currently logged in user
-    engranes = Reto.objects.filter("0")#engranes=request.user) # fetch it from database then render it to the template
+    engranes = Reto.objects.filter(0)#engranes=request.user) # fetch it from database then render it to the template
 
     return render(request, 'roboworld_app/cuenta_usuario.html', {"engranes":engranes})
 
