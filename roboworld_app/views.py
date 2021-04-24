57e8a1596f340a374c40bb2282bad470a58a232e
Christian González <a01657929@itesm.mx>
2021-04-23 22:30:03 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -46,7 +46,7 @@ def grafica1(request):
     resultados = Reto.objects.all()
 
     for i in resultados:
-        x = i.user.get("username")
+        x = i.user.username
         y = i.engranes
         data.append([x,y])
     
