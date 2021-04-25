c290edb8177efa0c85998a85e294d7e1f19e1745
Christian González <a01657929@itesm.mx>
2021-04-24 21:21:32 -0500

    automatic login

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -35,7 +35,7 @@ def cuenta_usuario(request):
 
 #---------------------------------------------- FINALES FUNCIONANDO ----------------------------------------------#
 
-
+'''
 def register(request):
     if request.method == 'POST':
         f = CustomUserCreationForm(request.POST)
@@ -51,6 +51,21 @@ def register(request):
         f = CustomUserCreationForm()
     return render(request, 'roboworld_app/register.html', {'form': f})
 
+
+from django.contrib.auth import authenticate, login
+'''
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            messages.info(request, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            return render(request, "roboworld_app/index.html")
 
 
 
