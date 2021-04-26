a6a30f13cc4ef0a9b14df27575d4a5f93cda9190
rebegula <rebeca_gul@outlook.es>
2021-04-26 16:00:46 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -226,7 +226,7 @@ Engranes
 def engranes(request):
     body_unicode = request.body.decode('utf-8')
     body_json = loads(body_unicode) #convertir de string a JSON
-    sesionObtenida = body_json['sesion']
+    sesionObtenida = body_json['ses']
     sesionObtained= ""
     numero = ""
 
