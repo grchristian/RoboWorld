90582bc7d22a7cc259c75e8598db557382b96669
rebegula <rebeca_gul@outlook.es>
2021-04-27 10:35:22 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -177,7 +177,7 @@ def Level(request):
     body_json = loads(body_unicode) #convertir de string a JSON
     numero_nivel = body_json['nivel']
     level_number = numero_nivel
-    enemigo = Level.objects(enemigo=enemigo)
+    enemigo = "mago"
     dificultad = ""
     duracion_individual=""
 
