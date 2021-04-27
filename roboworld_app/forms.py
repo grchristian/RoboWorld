588586eceec8f0de742409d7a65ba8dfb878379a
Christian González <a01657929@itesm.mx>
2021-04-27 16:15:46 -0500

    profile 2

--- roboworld_app/forms.py
+++ roboworld_app/forms.py
@@ -43,17 +43,3 @@ class CustomUserCreationForm(forms.Form):
 
 
 
-
-'''
-'''
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = Profile
-        fields = ('url', 'location', 'company')
-'''
-'''
