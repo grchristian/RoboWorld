244d70dab58d23784e7fddfa920b1bf1d2da035f
rebegula <rebeca_gul@outlook.es>
2021-04-27 12:42:07 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -417,7 +417,7 @@ def engranes(request):
     body_unicode = request.body.decode('utf-8')
     body_json = loads(body_unicode) #convertir de string a JSON
     Numero_engranes = body_json['engranes']
-    resultados = engranes.objects.filter(sessionObtained=Numero_engranes)  #select * from Reto where nombre = jugador_nombre
+    resultados = Engranes.objects.filter(sessionObtained=Numero_engranes)  #select * from Reto where nombre = jugador_nombre
     sessionObtained= resultados[0].sessionObtained
     numero = resultados[0].numero
     
