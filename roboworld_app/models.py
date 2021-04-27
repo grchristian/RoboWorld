0bfaaf2162865b1fd92f2f35c05a07e1648d35f2
Christian González <a01657929@itesm.mx>
2021-04-27 16:40:44 -0500

    profile models

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -7,7 +7,12 @@ from django.conf import settings
 
 '''
 '''
+
 class perfil(AbstractUser):
+    usuario = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
