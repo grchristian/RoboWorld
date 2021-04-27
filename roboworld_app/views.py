9ac1064e500045d404c3ad7279f8444033ebfbeb
rebegula <rebeca_gul@outlook.es>
2021-04-27 10:36:07 -0500

    Update views.py

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -177,9 +177,9 @@ def Level(request):
     body_json = loads(body_unicode) #convertir de string a JSON
     numero_nivel = body_json['nivel']
     level_number = numero_nivel
-    enemigo = "mago"
-    dificultad = ""
-    duracion_individual=""
+    #enemigo = "mago"
+    #dificultad = ""
+    #duracion_individual=""
 
     try:
         connection = psycopg2.connect(
