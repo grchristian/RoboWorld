6d5591d21784cb7ecbfb5a891301fb0318a64201
Christian González <a01657929@itesm.mx>
2021-04-27 16:24:50 -0500

    profile models

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -11,7 +11,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_de_usuario = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
