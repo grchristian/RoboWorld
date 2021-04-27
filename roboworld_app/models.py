d95d4e5abafc5e682f8c5c12c49a85d434a584a5
Christian González <a01657929@itesm.mx>
2021-04-27 16:45:39 -0500

    added profile model

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -15,6 +15,10 @@ class Reto(models.Model):
     sesion_iniciada_dia=models.IntegerField(null=True)
     sesion_iniciada_mes=models.IntegerField(null=True)
 
+class Reto(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    genero = models.CharField(max_length=1, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
 
 class Level(models.Model):
     #Nivelid = models.AutoField(primary_key=True)
