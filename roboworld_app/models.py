e332b3162544edf08d728a508826e2480fc23abf
Christian González <a01657929@itesm.mx>
2021-04-27 16:43:16 -0500

    profile model deleted

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -2,26 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User, AbstractUser
 from django.conf import settings
 
-
-
-
-'''
-'''
-
-class perfil(AbstractUser):
-    usuario = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-'''
-'''
-
-
-
-
 class Reto(models.Model):
     id_de_usuario = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True)
     minutos_jugados = models.IntegerField(null=True)
