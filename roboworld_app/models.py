cde2a0285cf66398a8919e3df17b7028561bb834
Christian González <a01657929@itesm.mx>
2021-04-27 15:59:54 -0500

    added user profile to models 1

--- roboworld_app/models.py
+++ roboworld_app/models.py
@@ -2,6 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
 
+
+
+
+'''
+'''
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+'''
+'''
+
+
+
+
 class Reto(models.Model):
     id_de_usuario = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True)
     minutos_jugados = models.IntegerField(null=True)
