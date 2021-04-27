d25f45945c4cc70e976b2a49dd7dab710318643c
Christian González <a01657929@itesm.mx>
2021-04-27 16:10:20 -0500

    profile test 1

--- roboworld_app/views.py
+++ roboworld_app/views.py
@@ -54,6 +54,40 @@ def register(request):
 
 #------------------------------------------------------- FINALES FUNCIONANDO -------------------------------------------------------#
 
+
+'''
+'''
+def update_profile(request, user_id):
+    user = User.objects.get(pk=user_id)
+    user.profile.bio = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit...'
+    user.save()
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('settings:profile')
+        else:
+            messages.error(request, _('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+'''
+'''
+
+
+
 #funciona pero hay que darle mas formato
 def grafica1(request):
     data = [['Nombre', 'Engranes recolectados']]
