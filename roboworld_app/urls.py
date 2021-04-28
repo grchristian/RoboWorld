42ec665a6fd6285d79b476e9ab5934f407d246ea
rebegula <rebeca_gul@outlook.es>
2021-04-28 10:29:49 -0500

    Update urls.py

--- roboworld_app/urls.py
+++ roboworld_app/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('graficaExito/', views.graficaExito, name="graficaExito"),
     path('graficaMinimo/', views.graficaMinimo, name="graficaMinimo"),
     path('graficaMaximo/', views.graficaMaximo, name="graficaMaximo"),
-    path('graficaNiveLlego/', views.graficaNivelLlego, name="graficaNiveLlego"),
+    path('graficaNivelLlego/', views.graficaNivelLlego, name="graficaNivelLlego"),
     path('graficaGenero/', views.graficaGenero, name="graficaGenero"),
     path('graficaTop5/', views.graficaTop5, name="graficaTop5"),
     #------------------------------ GRÁFICAS ------------------------------
